f11f368be45a1426f3c8a43c88f3f05166638373
Zahaouma <marouane.zahaoui@eurafric-information.com>
2024-11-27 09:56:53 +0100

    add some changes

--- angular-ssr/src/app/user/user.service.ts
+++ angular-ssr/src/app/user/user.service.ts
@@ -1,17 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+const USERS_KEY = 'users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<any> {
-    return this.http.get(this.apiUrl);
+  getUsers(): Observable<any[]> {
+    let cachedUsers = localStorage.getItem(USERS_KEY);
+
+    if (cachedUsers) {
+      return of(JSON.parse(cachedUsers));
+    } else {
+      return this.http.get<any[]>(this.apiUrl).pipe(
+        tap((users) => {
+          localStorage.setItem(USERS_KEY, JSON.stringify(users));
+        })
+      );
+    }
   }
 }
